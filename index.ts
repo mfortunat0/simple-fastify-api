25cc3d5951ba36294916fe61e9a2359ef84a2b95
matheus <matheus.xmaz10@gmail.com>
2022-01-07 20:33:41 -0300

    add post route

--- index.ts
+++ index.ts
@@ -1,8 +1,40 @@
 import Fastify from "fastify";
 const fastify = Fastify({ logger: true });
+import { v4 as uuidv4 } from "uuid";
 
-fastify.get("/items", (request, reply) => {
-  reply.send("Hello world");
+type Tool = {
+  id: string;
+  name: string;
+};
+
+interface IToolsRouteParams {
+  id: string;
+}
+
+interface IToolsRouteBody {
+  name: string;
+}
+
+const tools: Tool[] = [];
+
+fastify.get("/tools", (request, reply) => {
+  reply.send(tools);
+});
+
+fastify.get<{ Params: IToolsRouteParams }>("/tools/:id", (request, reply) => {
+  const { id } = request.params;
+  const tool = tools.find((tool) => tool.id === id);
+  reply.send(tool);
+});
+
+fastify.post<{ Body: IToolsRouteBody }>("/tools", (request, reply) => {
+  const { name } = request.body;
+  const tool: Tool = {
+    name,
+    id: uuidv4(),
+  };
+  tools.push(tool);
+  reply.send(tool);
 });
 
 const start = async () => {
