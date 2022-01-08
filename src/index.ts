c7de89f44cec61829453f2446139baf03327d35f
matheus <matheus.xmaz10@gmail.com>
2022-01-07 21:38:44 -0300

    add swagger docs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import Fastify from "fastify";
+import fastifySwagger from "fastify-swagger";
 const fastify = Fastify({ logger: true });
 import { ToolsRoutes } from "./controllers/ToolController";
+fastify.register(fastifySwagger, {
+  exposeRoute: true,
+  routePrefix: "/docs",
+  swagger: {
+    info: {
+      title: "fastify-tools-api",
+      version: "3",
+    },
+  },
+});
 fastify.register(ToolsRoutes);
 
 const start = async () => {
