fd734f975612006382e34633300bc15c24279fa5
matheus <matheus.xmaz10@gmail.com>
2022-01-07 21:30:57 -0300

    add schemas to controller

--- src/controllers/ToolController.ts
+++ src/controllers/ToolController.ts
@@ -1,6 +1,33 @@
 import { FastifyInstance, FastifyPluginOptions } from "fastify";
 import { v4 as uuidv4 } from "uuid";
 
+const tool = {
+  type: "object",
+  properties: {
+    id: { type: "string" },
+    name: { type: "string" },
+  },
+};
+
+const getToolsOptions = {
+  schema: {
+    response: {
+      200: {
+        type: "array",
+        items: tool,
+      },
+    },
+  },
+};
+
+const getToolOptions = {
+  schema: {
+    response: {
+      200: tool,
+    },
+  },
+};
+
 const ToolsRoutes = (
   fastify: FastifyInstance,
   options: FastifyPluginOptions,
@@ -21,15 +48,19 @@ const ToolsRoutes = (
 
   const tools: Tool[] = [];
 
-  fastify.get("/tools", (request, reply) => {
+  fastify.get("/tools", getToolsOptions, (request, reply) => {
     reply.send(tools);
   });
 
-  fastify.get<{ Params: IToolsRouteParams }>("/tools/:id", (request, reply) => {
-    const { id } = request.params;
-    const tool = tools.find((tool) => tool.id === id);
-    reply.send(tool);
-  });
+  fastify.get<{ Params: IToolsRouteParams }>(
+    "/tools/:id",
+    getToolOptions,
+    (request, reply) => {
+      const { id } = request.params;
+      const tool = tools.find((tool) => tool.id === id);
+      reply.send(tool);
+    }
+  );
 
   fastify.post<{ Body: IToolsRouteBody }>("/tools", (request, reply) => {
     const { name } = request.body;
